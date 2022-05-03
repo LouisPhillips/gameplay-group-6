6d9849364d06d21c49bc2a84d5b780ed491111d0
Dheeraj Dhoj Karki <dheeraj2.karki@live.uwe.ac.uk>
2022-05-03 04:01:19 +0100

    Fixed up Cutscene not reverting to Player Camera

--- gameplay-programming-group-6/Assets/Scripts/PlayerMovement/IntroScript.cs
+++ gameplay-programming-group-6/Assets/Scripts/PlayerMovement/IntroScript.cs
@@ -3,27 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class CutsceneSwitch : MonoBehaviour
+public class IntroScript : MonoBehaviour
 {
-
-    public GameObject TimelineObject;
-    public PlayableDirector director;
     public Camera CutsceneCamera;
     public Camera PlayerCamera;
     public bool used = false;
     public float timelineduration;
 
-    void Awake()
-    {
-        director = TimelineObject.GetComponent<PlayableDirector>();
-        CutsceneCamera.enabled = false;
-    }
 
-    public void PlayTimeline()
+    // Start is called before the first frame update
+    void Awake()
     {
         CutsceneCamera.enabled = true;
         PlayerCamera.enabled = false;
-        director.Play();
+
         StartCoroutine(TimeDelay());
     }
 

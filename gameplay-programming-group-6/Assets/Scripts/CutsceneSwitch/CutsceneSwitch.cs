245da0ab94849f3202bbbb57aad185a517eecab9
Dheeraj Dhoj Karki <dheeraj2.karki@live.uwe.ac.uk>
2022-05-03 04:35:58 +0100

    Wired up Switches to work with Player Controller when you attack them

--- gameplay-programming-group-6/Assets/Scripts/CutsceneSwitch/CutsceneSwitch.cs
+++ gameplay-programming-group-6/Assets/Scripts/CutsceneSwitch/CutsceneSwitch.cs
@@ -8,15 +8,22 @@ public class CutsceneSwitch : MonoBehaviour
 
     public GameObject TimelineObject;
     public PlayableDirector director;
+
+    public GameObject BossObject;
+    public PlayableDirector BossDirector;
+
     public Camera CutsceneCamera;
     public Camera PlayerCamera;
     public bool used = false;
     public float timelineduration;
 
+    public int SwitchCount;
+
     void Awake()
     {
         director = TimelineObject.GetComponent<PlayableDirector>();
         CutsceneCamera.enabled = false;
+        SwitchCount = 0;
     }
 
     public void PlayTimeline()
@@ -25,11 +32,29 @@ public class CutsceneSwitch : MonoBehaviour
         PlayerCamera.enabled = false;
         director.Play();
         StartCoroutine(TimeDelay());
+
+        if (SwitchCount == 3)
+        {
+            BossTimeline();
+        }
+    }
+
+    public void BossTimeline()
+    {
+        BossDirector.Play();
+        StartCoroutine(BossDelay());
     }
 
     IEnumerator TimeDelay()
     {
         yield return new WaitForSeconds(timelineduration);
+        PlayerCamera.enabled = true;
+        CutsceneCamera.enabled = false;
+    }
+
+    IEnumerator BossDelay()
+    {
+        yield return new WaitForSeconds(4);
         PlayerCamera.enabled = true;
         CutsceneCamera.enabled = false;
     }
--- gameplay-programming-group-6/Assets/Scripts/PlayerMovement/Attacking.cs
+++ gameplay-programming-group-6/Assets/Scripts/PlayerMovement/Attacking.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 
 public class Attacking : MonoBehaviour
 {
+    private Rigidbody sphererigidbody;
     public Animator animator;
     public Transform attackPoint;
     public LayerMask enemyLayers;
@@ -13,12 +14,18 @@ public class Attacking : MonoBehaviour
     public float attackRange = 0.5f;
     public int attackDamage = 1;
 
+    public bool atSwitch = false;
+    public GameObject CurSwitch;
+    public CutsceneSwitch SwitchInstance;
+    public int SwitchLimit;
+
     bool attacking;
 
     PlayerControls controls;
     private void Awake()
     {
         controls = new PlayerControls();
+        sphererigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -29,7 +36,14 @@ public class Attacking : MonoBehaviour
     {
         //play animation
         animator.SetTrigger("Attack");
-        //deteckt enemies
+
+        if (atSwitch)
+        {
+            SwitchInstance.PlayTimeline();
+            SwitchInstance.SwitchCount += 1;
+        }
+
+        //detect enemies
         Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
         Collider[] bossHit = Physics.OverlapSphere(attackPoint.position, attackDamage, bossLayer);
         Collider[] newSlimes = Physics.OverlapSphere(attackPoint.position, attackDamage, newLayer);
@@ -61,5 +75,15 @@ public class Attacking : MonoBehaviour
             return;
         }
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Switch")
+        {
+            atSwitch = true;
+            CurSwitch = other.gameObject;
+            SwitchInstance = CurSwitch.GetComponent<CutsceneSwitch>();
+        }
     }
 }
